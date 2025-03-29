d49e02275cb844d7f9fcb01a4f19278a0e6b5576
Mathijs Bakker <204105@stmichaelcollege.nl>
2025-03-29 15:48:28 +0100

    betere sprong toegevoegd en platform waar je naartoe kan springen

--- player_decie.gd
+++ player_decie.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+var SPEED = 2.0
+var JUMP_VELOCITY = 9.0
 var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
@@ -11,16 +13,20 @@ var pitch_input := 0.0
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func is_on_floor() -> bool:
+	# Simple check: If vertical velocity is small, assume the player is on the ground
+	return abs(linear_velocity.y) < 0.05
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-	input.y = Input.get_axis("shift", "space")
 	
+	if is_on_floor() and Input.is_action_just_pressed("space"):
+		apply_central_impulse(Vector3(0, JUMP_VELOCITY, 0))
 	
-	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * SPEED * 1200.0 * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -29,8 +35,8 @@ func _process(delta: float) -> void:
 	pitch_pivot.rotate_x(pitch_input)
 	pitch_pivot.rotation.x = clamp(
 		pitch_pivot.rotation.x, 
-		-0.5,
-		0.5
+		-0.8,
+		0.6
 	)
 	twist_input = 0.0
 	pitch_input = 0.0
